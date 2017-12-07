15a856d7c067e274a409261d4f357a865db75cc2
fsps60312 <fsps60312@yahoo.com.tw>
2017-12-07 23:42:31 +0800

    借我錢

--- Dialogs/Main/Main.cs
+++ Dialogs/Main/Main.cs
@@ -48,6 +48,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                         case "Code風景區": await context.PostAsync("很棒的名字，不覺得嗎？XD<br/>然後，我的英文名字是「code scenic」哦，Google看看！<br/>總之，像欣賞風景一樣快樂的探索程式之美吧！");break;
                         case "傳一則貼文的網址(?)": await context.PostAsync("吼～不是真的要你說這句話啦！<br/>是你要傳一則貼文的網址給我～><"); break;
                         case "說話": await context.PostAsync("話"); break;
+                        case "借我錢":await context.PostAsync("不要");break;
                         case "我要說甚麼?":
                             {
                                 var commands = Constants.Commands.ListCommands();
