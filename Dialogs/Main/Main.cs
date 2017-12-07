3560ac099d80a214e1cf63637373c2cda4ae918d
fsps60312 <fsps60312@yahoo.com.tw>
2017-12-07 23:52:08 +0800

    不要

--- Dialogs/Main/Main.cs
+++ Dialogs/Main/Main.cs
@@ -45,10 +45,11 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                     switch (messageText)
                     {
                         case "code風景區":
-                        case "Code風景區": await context.PostAsync("很棒的名字，不覺得嗎？XD<br/>然後，我的英文名字是「code scenic」哦，Google看看！<br/>總之，像欣賞風景一樣快樂的探索程式之美吧！");break;
+                        case "Code風景區": await context.PostAsync("很棒的名字，不覺得嗎？XD<br/>然後，我的英文名字是「code scenic」哦，Google看看！<br/>總之，像欣賞風景一樣快樂的探索程式之美吧！"); break;
                         case "傳一則貼文的網址(?)": await context.PostAsync("吼～不是真的要你說這句話啦！<br/>是你要傳一則貼文的網址給我～><"); break;
                         case "說話": await context.PostAsync("話"); break;
-                        case "借我錢":await context.PostAsync("我沒錢><");break;
+                        case "借我錢": await context.PostAsync("我沒錢><"); break;
+                        case "不要": await context.PostAsync("好吧，你壞壞><"); break;
                         case "我要說甚麼":
                             {
                                 var commands = Constants.Commands.ListCommands();
@@ -112,7 +113,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                                 {
                                     case 0: break;
                                     case 1: msg = "你說了「" + msg + "」"; break;
-                                    case 2: msg = "好啦，"+ msg; break;
+                                    case 2: msg = "好啦，" + msg; break;
                                     case 3: msg = msg + " XDD"; break;
                                     case 4: msg = msg + " www"; break;
                                     case 5: msg = msg + " ^_^"; break;
